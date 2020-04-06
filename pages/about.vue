be7c31a47010154ae1d6c532c5128a7e3b04e866
Jacque Schrag <jnschrag@gmail.com>
2020-04-06 16:28:17 -0400

    Create page layout component with slots for header & content
    - Starts #16 by creating page layout component, but remains unfinished until site header & site footer components are added.
    - Applied page component to Analysis page (#25)
    - Applied page component to About page (#20)

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <Page title="About" :content-has-bg="true">
+    <div class="content-wrapper">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero odit aut
+        similique quos magnam ab rem explicabo. Non, soluta officia at
+        quibusdam, laborum ducimus enim quos dolorum a unde velit.
+      </p>
+    </div>
+  </Page>
+</template>
+
+<script>
+import Page from '~/layout/page'
+
+export default {
+  layout: 'layout',
+  components: {
+    Page
+  }
+}
+</script>
--- pages/analysis/index.vue
+++ pages/analysis/index.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
+  <Page title="Analysis">
+    <div slot="header">
+      <p class="page__desc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat ornare
+        dictum ut arcu diam morbi mi rutrum aliquet. Varius molestie eros
+        scelerisque convallis ornare sociis fusce mauris. Vitae ut etiam purus
+        in mattis tristique feugiat tellus lacinia.
+      </p>
+    </div>
     <PostBlock v-for="post in posts" :key="post.title" :data="post" />
-  </div>
+  </Page>
 </template>
 
 <script>
+import Page from '~/layout/page'
 import Posts from '~/content/data/analysis.json'
 import PostBlock from '~/components/global/PostBlock.vue'
 
 export default {
   layout: 'layout',
   components: {
+    Page,
     PostBlock
   },
   computed: {
@@ -20,3 +30,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.page__desc {
+  max-width: $size__content-max-width;
+  margin-right: auto;
+  margin-left: auto;
+  color: $color-white-180;
+  @extend %font-ui-text-lg-oxygen;
+}
+</style>
