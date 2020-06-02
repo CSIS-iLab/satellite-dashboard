1c108571612d2a0f62ad0fc5da7ddf169654137e
Jacque Schrag <jnschrag@gmail.com>
2020-06-02 11:21:31 -0400

    Refactored copyright component & added Footer component

---
+++ components/global/Copyright.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="copyright">
+    &copy; {{ year }}
+    <span>
+      <nuxt-link to="/about#license">Licensing Information</nuxt-link>
+    </span>
+    <span>
+      <a href="https://www.csis.org/privacy-policy">Privacy Policy</a>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Copyright',
+  data: function() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/css/components/copyright';
+</style>
---
+++ components/global/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <Copyright />
+  </footer>
+</template>
+
+<script>
+import Copyright from '~/components/global/Copyright.vue'
+
+export default {
+  components: {
+    Copyright
+  }
+}
+</script>
+
+<style lang="scss"></style>
