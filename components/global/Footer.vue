cff547deaa52f95011e1e08e5dd1b96b18873029
mariel-delagarza <mariel.code@gmail.com>
2020-06-09 11:35:59 -0400

    Initial component draft - render functioning twitter and email link, twitter and email icons

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,18 +1,22 @@
 <template>
   <footer class="footer">
-    <Copyright />
     <Icon name="twitter" />
+    <Icon name="email" />
+    <Social />
+    <Copyright />
   </footer>
 </template>
 
 <script>
 import Copyright from '~/components/global/Copyright.vue'
 import Icon from '~/components/global/Icon.vue'
+import Social from '~/components/global/SocialMedia.vue'
 
 export default {
   components: {
     Copyright,
-    Icon
+    Icon,
+    Social
   }
 }
 </script>
---
+++ components/global/SocialMedia.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="social-media">
+    <a href="https://twitter.com/satdashboard">@satdashboard</a>
+    <a href="mailto: spacetranspraencyproject@gmail.com"
+      >spacetransparencyproject@gmail.com</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Social'
+}
+</script>
+
+<style lang="scss">
+@import '../assets/css/components/social-media';
+</style>
