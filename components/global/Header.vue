f8579b26e0890ce17cad4f5123aeabd15d8437b8
Jacque Schrag <jnschrag@gmail.com>
2020-06-09 11:59:58 -0400

    Setup visualizer layout on home page. Create placeholder Header component.

---
+++ components/global/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <h1 class="title">satellite-dashboard</h1>
+    <div class="links">
+      <a href="https://nuxtjs.org/" target="_blank">
+        Nuxt Documentation
+      </a>
+      <br />
+      <a href="https://github.com/nuxt/nuxt.js" target="_blank">
+        GitHub
+      </a>
+      <br />
+      <nuxt-link to="/about">About Page</nuxt-link>
+      <br />
+      <nuxt-link to="/analysis">Analysis</nuxt-link>
+    </div>
+  </header>
+</template>
+
+<style lang="scss"></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <SiteLogo />
-    Site Header
+    <Header />
     <SocialShare page-title="Foobar" />
     <Button :on-click="onClick">Test Content</Button>
     <Button :on-click="onClick" class="btn--sm">Smol Button</Button>
@@ -10,6 +10,7 @@
     <Button :on-click="onClick" class="btn--outlined">Outlined Button</Button>
     <Button :on-click="onClick" class="btn--light">Light Button</Button>
     <Button :on-click="onClick" class="btn--icon">X</Button>
+    <Header />
     <nuxt />
     <Footer />
   </div>
@@ -17,12 +18,14 @@
 
 <script>
 import SiteLogo from '~/components/global/SiteLogo.vue'
+import Header from '~/components/global/Header.vue'
 import Footer from '~/components/global/Footer.vue'
 import SocialShare from '~/components/global/SocialShare.vue'
 import Button from '~/components/global/Button.vue'
 
 export default {
   components: {
+    Header,
     Footer,
     SiteLogo,
     SocialShare,
