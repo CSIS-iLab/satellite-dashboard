d253ca409b888d77ec6c1f5ab58e9a7240286dd2
Lindsay Urchyk <urchykli@gmail.com>
2020-07-27 16:56:04 -0400

    Header menu and logo #9

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,10 +1,12 @@
 <template>
   <header class="header">
+    <Icon id="menu" class="icon header__menu" name="menu" />
     <nuxt-link to="/">
       <SiteLogo />
     </nuxt-link>
     <Navigation class="header__nav" name="header" />
     <SocialShare page-title="Sat Dash" />
+    <Social />
     <Copyright />
   </header>
 </template>
@@ -14,13 +16,15 @@ import SiteLogo from '~/components/global/SiteLogo.vue'
 import Navigation from '~/components/global/Navigation.vue'
 import SocialShare from '~/components/global/SocialShare.vue'
 import Copyright from '~/components/global/Copyright.vue'
+import Icon from '~/components/global/Icon.vue'
 
 export default {
   components: {
     Copyright,
     Navigation,
     SiteLogo,
-    SocialShare
+    SocialShare,
+    Icon
   }
 }
 </script>
