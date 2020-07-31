42add72e596568871d8488622e5856f75245ec1f
Lindsay Urchyk <urchykli@gmail.com>
2020-07-31 14:39:28 -0400

    Navigation link to visualizer

--- components/global/Navigation.vue
+++ components/global/Navigation.vue
@@ -1,11 +1,37 @@
 <template>
-  <ol>
-    <li></li>
+  <ol :class="parentClass">
+    <li class="visualizer">
+      <nuxt-link :to="nestedSlug" append>Visualizer</nuxt-link>
+    </li>
+    <li>Visualizer</li>
   </ol>
 </template>
 
 <script>
+import json from 'static/navigation.json'
 export default {
-  name: 'Navigation'
+  name: 'Navigation',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return { data: json }
+  },
+  computed: {
+    parentClass() {
+      return `nav-${this.name}`
+    },
+    nestedSlug() {
+      console.log(this.data)
+      return `${this.data.visualizer.url}`
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+@import '../assets/css/components/navigation';
+</style>
