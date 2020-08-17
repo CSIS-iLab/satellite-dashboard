5deeece787ee83c748f736baa0e4db627dcb1d3f
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-08-17 13:38:10 -0400

    Removed unecessary components, Reworked Footer.vue to include former components, Attempted minor styling with footer.scss

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,30 +1,42 @@
 <template>
   <footer class="footer">
     <SiteLogo />
-    <Paragraph />
+    <p class="footer__description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolores
+      modi adipisci inventore, nihil cumque. Cumque, sit ipsam? Est aperiam
+      doloremque esse nihil? Asperiores provident ullam incidunt qui nostrum
+      nam!
+    </p>
     <Social />
     <Navigation name="footer" />
-    <LearnMoreAbout />
+    <h2 class="footer__learnAboutMoreHeader">Learn More About</h2>
+    <ul class="footer__learnAboutMore">
+      <li>
+        <p><a href="">CSIS</a></p>
+      </li>
+      <li>
+        <p><a href="#">Secure World Foundation</a></p>
+      </li>
+      <li>
+        <p><a href="#">University of Texas at Austin</a></p>
+      </li>
+    </ul>
     <Copyright />
   </footer>
 </template>
 
 <script>
 import SiteLogo from '~/components/global/SiteLogo.vue'
-import Paragraph from '~/components/global/Paragraph.vue'
 import Copyright from '~/components/global/Copyright.vue'
 import Social from '~/components/global/SocialMedia.vue'
 import Navigation from '~/components/global/Navigation.vue'
-import LearnMoreAbout from '~/components/global/LearnMoreAbout.vue'
 
 export default {
   components: {
     SiteLogo,
-    Paragraph,
     Copyright,
     Social,
-    Navigation,
-    LearnMoreAbout
+    Navigation
   }
 }
 </script>
