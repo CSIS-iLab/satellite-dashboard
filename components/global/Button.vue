90b1aaf1bc4e6ee155c55c5daf0d9ff429ec4520
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-08-31 13:36:22 -0400

    Button styling, working on the switching between a and button tags.

---
+++ components/global/Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <content :is="type" :href="href" class="button btn" @click="onClick">
+    <slot />
+  </content>
+</template>
+
+<script>
+export default {
+  props: {
+    onClick: {
+      type: Function,
+      required: true
+    },
+    href: {
+      type: String,
+      default: null
+    },
+    to: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    type() {
+      if (this.href) {
+        return 'a'
+      } else {
+        return 'button'
+      }
+    }
+  }
+}
+</script>
+<!--<template>
+  <button class="btn" @click="onClick">
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>-->
+
+<style lang="scss">
+@import '../assets/css/components/button';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
     <SiteLogo />
     Site Header
     <SocialShare page-title="Foobar" />
+    <Button :on-click="onClick" />
     <nuxt />
     <Footer />
   </div>
@@ -12,12 +13,19 @@
 import SiteLogo from '~/components/global/SiteLogo.vue'
 import Footer from '~/components/global/Footer.vue'
 import SocialShare from '~/components/global/SocialShare.vue'
+import Button from '~/components/global/Button.vue'
 
 export default {
   components: {
     Footer,
     SiteLogo,
-    SocialShare
+    SocialShare,
+    Button
+  },
+  methods: {
+    onClick() {
+      console.log('Hello World')
+    }
   }
 }
 </script>
