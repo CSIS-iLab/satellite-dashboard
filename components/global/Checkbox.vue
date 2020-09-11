7fa57c84ed588db703868d225d73953cb4303155
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-09-11 15:19:36 -0400

    checkbox scaffolding

---
+++ components/global/Checkbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="input__wrapper">
+    <input id="jack" v-model="checkedNames" type="checkbox" value="Jack" />
+    <label for="jack">Jack</label>
+    <input id="john" v-model="checkedNames" type="checkbox" value="John" />
+    <label for="john">John</label>
+    <input id="mike" v-model="checkedNames" type="checkbox" value="Mike" />
+    <label for="mike">Mike</label>
+    <br />
+    <span>Checked names: {{ checkedNames }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      checkedNames: []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/css/components/forms';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
     <SocialShare page-title="Foobar" />
     <nuxt />
     <TextInput />
+    <Checkbox />
     <Footer />
   </div>
 </template>
@@ -14,13 +15,15 @@ import SiteLogo from '~/components/global/SiteLogo.vue'
 import Footer from '~/components/global/Footer.vue'
 import SocialShare from '~/components/global/SocialShare.vue'
 import TextInput from '~/components/global/TextInput.vue'
+import Checkbox from '~/components/global/Checkbox.vue'
 
 export default {
   components: {
     Footer,
     SiteLogo,
     SocialShare,
-    TextInput
+    TextInput,
+    Checkbox
   }
 }
 </script>
