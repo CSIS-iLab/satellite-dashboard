b3fb654bafcdde264c32aef5194ea4ed7e36cfc5
Jacque Schrag <jnschrag@gmail.com>
2020-11-03 11:33:43 -0500

    Add comments to code for visualization & update readme

--- components/visualizer/CesiumViewer.vue
+++ components/visualizer/CesiumViewer.vue
@@ -23,18 +23,19 @@
 </template>
 
 <script>
-import * as satellite from 'satellite.js'
+// import * as satellite from 'satellite.js' // Was used to create TLEs for propogation.
 
-var J2000Epoch = 2451545.0
-var JulYear = 365.25
-var JulCent = JulYear * 100
+// Set up constants needed for position calculations.
+const J2000Epoch = 2451545.0
+const JulYear = 365.25
+const JulCent = JulYear * 100
 
-var EGM96_mu = 3.986004415e14 // m^3/s^2
-var EGM96_REarth = 6378136.3 // m
+const EGM96_mu = 3.986004415e14 // m^3/s^2
+const EGM96_REarth = 6378136.3 // m
 
-var TwoPi = 2 * Math.PI
-var RadDeg = 180 / Math.PI
-var DegRad = Math.PI / 180
+const TwoPi = 2 * Math.PI
+const RadDeg = 180 / Math.PI
+const DegRad = Math.PI / 180
 
 export default {
   props: {
@@ -65,6 +66,7 @@ export default {
   },
   methods: {
     ready(cesiumInstance) {
+      // Set up the Cesium Viewer
       const { Cesium, viewer } = cesiumInstance
       const scene = viewer.scene
       const globe = scene.globe
@@ -86,19 +88,22 @@ export default {
         new Cesium.JulianDate()
       )
 
+      // Set up the current time and then load in the satellite objects.
       Cesium.Transforms.preloadIcrfFixed(
         new Cesium.TimeInterval({
           start: new Cesium.JulianDate(J2000Epoch - JulCent),
           stop: new Cesium.JulianDate(J2000Epoch + JulCent)
         })
       ).then(() => {
-        this.displayPoints(Cesium, viewer)
+        this.displayObjects(Cesium, viewer)
       })
     },
-    displayPoints(Cesium, viewer) {
+    displayObjects(Cesium, viewer) {
       let epjd = new Cesium.JulianDate()
       let CRFtoTRF = Cesium.Transforms.computeIcrfToFixedMatrix(this.SimStart)
 
+      // For each object, calculate its position & orbit
+      // Calculations pulled from: https://github.com/ut-astria/AstriaGraph/blob/master/main.js & https://github.com/ut-astria/AstriaGraph/blob/master/celemech.js
       for (let i = 0; i < this.satellites.length; i++) {
         const sat = this.satellites[i]
         const { id, orbitalDatum, name } = sat
@@ -111,6 +116,8 @@ export default {
 
         let elems = {}
 
+        // Format orbital elements
+        // Todo: Can this be refactored to happen elsewhere, such as in the database or when it is pulled into the app?
         for (let i = 0; i < elemsNames.length; i++) {
           const name = elemsNames[i]
           let value = datum[name].value
@@ -121,7 +128,8 @@ export default {
         }
 
         // TODO: Use actual epoch date
-        Cesium.JulianDate.fromIso8601('2020-06-13T22:00:02.000000Z', epjd)
+        // Cesium.JulianDate.fromIso8601('2020-06-13T22:00:02.000000Z', epjd)
+        Cesium.JulianDate.fromIso8601(elems.epoch, epjd)
         // console.log(elems.epoch)
         // Cesium.JulianDate.fromIso8601(elems.epoch, epjd)
         let t = Cesium.JulianDate.daysDifference(this.SimStop, epjd)
@@ -129,11 +137,15 @@ export default {
         elems.meanAnom = (elems.meanAnom + elems.mmo * t * 86400) % TwoPi
 
         // Calculate Orbit
+        // TODO: This is computationally expensive. If orbits are only visible when the user has selected the object, is there a better way to handle these calculations?
         let orbit = []
-        if (i === 0) {
-          orbit = this.calcOrbit(Cesium, viewer, CRFtoTRF, elems)
-        }
+        // if (i === 0) {
+        orbit = this.calcOrbit(Cesium, viewer, CRFtoTRF, elems)
+        console.log('orbit')
+        console.log(orbit)
+        // }
 
+        // TODO: Adjust the availability & position properties. See https://github.com/ut-astria/AstriaGraph/blob/master/main.js#L212-L220 for reference.
         viewer.entities.add({
           id: id,
           name: name.value,
@@ -162,6 +174,7 @@ export default {
         })
       }
     },
+    // Reference: https://github.com/ut-astria/AstriaGraph/blob/master/main.js#L228-L240
     updatePosition(Cesium, CsView, CRFtoTRF, elems) {
       // return function UpdateHelper() {
       var t = Cesium.JulianDate.secondsDifference(
@@ -176,6 +189,7 @@ export default {
       return eff
       // }
     },
+    // Reference: https://github.com/ut-astria/AstriaGraph/blob/master/celemech.js#L44
     eltocart(Cesium, ele, posonly = false, tol = 1e-6, MAXITER = 20) {
       var ecan = this.ecceanom(ele.meanAnom, ele.ecc, tol, MAXITER)
       var tran =
@@ -228,6 +242,7 @@ export default {
 
       return { pos: pos2, vel: vel }
     },
+    // Reference: https://github.com/ut-astria/AstriaGraph/blob/master/celemech.js#L30
     ecceanom(mean, ecc, tol, MAXITER) {
       var prev = mean
       for (let i = 1; i <= MAXITER; i++) {
@@ -240,6 +255,7 @@ export default {
 
       return NaN
     },
+    // Reference: https://github.com/ut-astria/AstriaGraph/blob/master/main.js#L242
     calcOrbit(Cesium, viewer, CRFtoTRF, elems) {
       let car = new Cesium.Cartographic()
       let Y = new Cesium.Cartesian3()
@@ -266,121 +282,122 @@ export default {
         arr.push(car.longitude, car.latitude, car.height)
       }
       return arr
-    },
-    calcTLE(sat) {
-      const { id, orbitalDatum } = sat
-      const items = orbitalDatum.items
-      if (items.length <= 0) {
-        return
-      }
-      const datum = items[0]
-      const elemsNames = Object.keys(datum)
-
-      let elems = {}
-
-      for (let i = 0; i < elemsNames.length; i++) {
-        const name = elemsNames[i]
-        let value = datum[name].value
-        if (name !== 'epoch') {
-          value = +value
-        }
-        elems[name] = value
-      }
-
-      const epoch = elems.epoch.split('T')
-      const epochDate = new Date(epoch[0])
-      const year = epochDate
-        .getFullYear()
-        .toString()
-        .substr(-2)
-      const daysIntoYear = this.daysIntoYear(epochDate)
-
-      const fractionOfDay = this.fractionOfDay(epoch[1])
-
-      const inclination = this.radiansToDegrees(elems.inc)
-      const RAAN = this.radiansToDegrees(elems.RAAN)
-      const argP = this.radiansToDegrees(elems.argP)
-      const meanAnom = this.radiansToDegrees(elems.meanAnom)
-      const meanMotion = this.calcMeanMotion(elems.SMA)
-      const ecc = this.calcEcc(elems.ecc)
-
-      const line1 = `1 ${id}U 00000AAA ${year}${daysIntoYear}${fractionOfDay} -.00000000  00000-0 -00000-0 0  0000`
-      const line2 = `2 ${id} ${inclination} ${RAAN} ${ecc} ${argP} ${meanAnom} ${meanMotion}000000`
-
-      return [line1, line2]
-    },
-    daysIntoYear(date) {
-      return (
-        (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
-          Date.UTC(date.getFullYear(), 0, 0)) /
-        24 /
-        60 /
-        60 /
-        1000
-      )
-    },
-    fractionOfDay(time) {
-      const value = Number.parseFloat(
-        this.hmsToSecondsOnly(time) / 86400
-      ).toFixed(8)
-
-      return value.substr(1)
-    },
-    hmsToSecondsOnly(str) {
-      var p = str.split(':'),
-        s = 0,
-        m = 1
-
-      while (p.length > 0) {
-        s += m * parseInt(p.pop(), 10)
-        m *= 60
-      }
-
-      return s
-    },
-    radiansToDegrees(value) {
-      let degrees = value * (180 / Math.PI)
-
-      if (degrees < 0) {
-        degrees += 360
-      }
-
-      let prefix = ''
-      if (degrees < 100 && degrees >= 10) {
-        prefix = '0'
-      } else if (degrees < 10) {
-        prefix = '00'
-      }
-
-      return prefix + Number.parseFloat(degrees).toFixed(4)
-    },
-    calcMeanMotion(value) {
-      const mu = 3.986004415e14
-      const meanMotion =
-        Math.sqrt(mu / Math.pow(value, 3)) * (86400 / (2 * Math.PI))
-
-      let prefix = ''
-      if (meanMotion < 10) {
-        prefix = ' '
-      }
-
-      return prefix + Number.parseFloat(meanMotion).toFixed(8)
-    },
-    calcEcc(ecc) {
-      const value = Number.parseFloat(
-        Math.round(ecc * Math.pow(10, 7))
-      ).toString()
-      return value.padStart(7, '0')
-    },
-    arrayToCartesianArray(array, Cesium) {
-      let newArray = []
-      for (let i = 0; i < array.length; i += 3) {
-        let p = new Cesium.Cartesian3(array[i], array[i + 1], array[i + 2])
-        newArray.push(p)
-      }
-
-      return newArray
     }
+    // Below code was used to generate TLEs to satellite.js could be used for propagation.
+    // calcTLE(sat) {
+    //   const { id, orbitalDatum } = sat
+    //   const items = orbitalDatum.items
+    //   if (items.length <= 0) {
+    //     return
+    //   }
+    //   const datum = items[0]
+    //   const elemsNames = Object.keys(datum)
+
+    //   let elems = {}
+
+    //   for (let i = 0; i < elemsNames.length; i++) {
+    //     const name = elemsNames[i]
+    //     let value = datum[name].value
+    //     if (name !== 'epoch') {
+    //       value = +value
+    //     }
+    //     elems[name] = value
+    //   }
+
+    //   const epoch = elems.epoch.split('T')
+    //   const epochDate = new Date(epoch[0])
+    //   const year = epochDate
+    //     .getFullYear()
+    //     .toString()
+    //     .substr(-2)
+    //   const daysIntoYear = this.daysIntoYear(epochDate)
+
+    //   const fractionOfDay = this.fractionOfDay(epoch[1])
+
+    //   const inclination = this.radiansToDegrees(elems.inc)
+    //   const RAAN = this.radiansToDegrees(elems.RAAN)
+    //   const argP = this.radiansToDegrees(elems.argP)
+    //   const meanAnom = this.radiansToDegrees(elems.meanAnom)
+    //   const meanMotion = this.calcMeanMotion(elems.SMA)
+    //   const ecc = this.calcEcc(elems.ecc)
+
+    //   const line1 = `1 ${id}U 00000AAA ${year}${daysIntoYear}${fractionOfDay} -.00000000  00000-0 -00000-0 0  0000`
+    //   const line2 = `2 ${id} ${inclination} ${RAAN} ${ecc} ${argP} ${meanAnom} ${meanMotion}000000`
+
+    //   return [line1, line2]
+    // },
+    // daysIntoYear(date) {
+    //   return (
+    //     (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+    //       Date.UTC(date.getFullYear(), 0, 0)) /
+    //     24 /
+    //     60 /
+    //     60 /
+    //     1000
+    //   )
+    // },
+    // fractionOfDay(time) {
+    //   const value = Number.parseFloat(
+    //     this.hmsToSecondsOnly(time) / 86400
+    //   ).toFixed(8)
+
+    //   return value.substr(1)
+    // },
+    // hmsToSecondsOnly(str) {
+    //   var p = str.split(':'),
+    //     s = 0,
+    //     m = 1
+
+    //   while (p.length > 0) {
+    //     s += m * parseInt(p.pop(), 10)
+    //     m *= 60
+    //   }
+
+    //   return s
+    // },
+    // radiansToDegrees(value) {
+    //   let degrees = value * (180 / Math.PI)
+
+    //   if (degrees < 0) {
+    //     degrees += 360
+    //   }
+
+    //   let prefix = ''
+    //   if (degrees < 100 && degrees >= 10) {
+    //     prefix = '0'
+    //   } else if (degrees < 10) {
+    //     prefix = '00'
+    //   }
+
+    //   return prefix + Number.parseFloat(degrees).toFixed(4)
+    // },
+    // calcMeanMotion(value) {
+    //   const mu = 3.986004415e14
+    //   const meanMotion =
+    //     Math.sqrt(mu / Math.pow(value, 3)) * (86400 / (2 * Math.PI))
+
+    //   let prefix = ''
+    //   if (meanMotion < 10) {
+    //     prefix = ' '
+    //   }
+
+    //   return prefix + Number.parseFloat(meanMotion).toFixed(8)
+    // },
+    // calcEcc(ecc) {
+    //   const value = Number.parseFloat(
+    //     Math.round(ecc * Math.pow(10, 7))
+    //   ).toString()
+    //   return value.padStart(7, '0')
+    // },
+    // arrayToCartesianArray(array, Cesium) {
+    //   let newArray = []
+    //   for (let i = 0; i < array.length; i += 3) {
+    //     let p = new Cesium.Cartesian3(array[i], array[i + 1], array[i + 2])
+    //     newArray.push(p)
+    //   }
+
+    //   return newArray
+    // }
   }
 }
 
