b6051b76b09d719b8a912a42c15e1b883974f448
Jacque Schrag <jnschrag@gmail.com>
2020-12-04 14:08:22 -0500

    Remove early return in getSatellites

--- store/satellites.js
+++ store/satellites.js
@@ -36,8 +36,6 @@ export const actions = {
    * Pulls Satellite from WordPress database and stores them in state so they can be filtered.
    */
   async getSatellites({ state, commit }) {
-    // if (state.satellites.length) return
-
     try {
       let satellites = await fetch(
         `${siteURL}/wp-json/satdash/v1/satellites?date=${state.targetDate}`
