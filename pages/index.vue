46219643c4ccc842debb3fbb0490590cc8e185de
Jacque Schrag <jnschrag@gmail.com>
2020-12-10 11:53:35 -0500

    Update visualizer layout to not use page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,22 @@
 <template>
-  <Page title="Analysis">
-    <div slot="header">
-      <p class="page__desc">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat ornare
-        dictum ut arcu diam morbi mi rutrum aliquet. Varius molestie eros
-        scelerisque convallis ornare sociis fusce mauris. Vitae ut etiam purus
-        in mattis tristique feugiat tellus lacinia.
-      </p>
-    </div>
-    <div>
-      <template v-if="loading"> Loading... </template>
+  <div>
+    <template v-if="loading"> Loading... </template>
 
-      <template v-else>
-        <CesiumViewer :satellites="satellites" :selected-date="targetDate" />
+    <template v-else>
+      <CesiumViewer :satellites="satellites" :selected-date="targetDate" />
 
-        <Timeline :selected-date="targetDate" />
-        <!--<table border="1" borderColor="#fff" cellSpacing="0">
-          <thead>
-            <tr>
-              <td>Catalog Id</td>
-              <td>Name</td>
-              <td>Orbital Data</td>
-            </tr>
-          </thead>
-          <tr v-for="sat in satellites" :key="sat.id">
-            <td>{{ sat.catalog_id }}</td>
-            <td>{{ sat.source1.Name }}</td>
-            <td>{{ sat.orbital }}</td>
-          </tr>
-        </table>-->
-      </template>
-    </div>
-  </Page>
+      <Timeline :selected-date="targetDate" />
+    </template>
+  </div>
 </template>
 
 <script>
 import CesiumViewer from '~/components/visualizer/CesiumViewer'
 import Timeline from '~/components/timeline/Timeline'
-import Page from '~/layout/page'
 
 export default {
   layout: 'visualizer',
   components: {
-    Page,
     CesiumViewer,
     Timeline
   },
