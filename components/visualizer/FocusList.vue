b44f66a8be64e196eaf83f7f1a0903050980107b
Jacque Schrag <jnschrag@gmail.com>
2021-01-18 11:45:12 -0500

    Display list of focused satellites in focus list

--- components/visualizer/FocusList.vue
+++ components/visualizer/FocusList.vue
@@ -12,17 +12,71 @@
       Hide object labels<br />
     </div>
     <div>
-      # of objects saved<br />
+      {{ focusedSatellitesCount }} objects saved<br />
       Edit button
     </div>
-    <div>
-      List of Items in Focus
-    </div>
+    <ul class="sat-list" role="list">
+      <li
+        v-for="item in focusedItems"
+        :key="item"
+        class="sat__basic sat__basic--status"
+        :data-status="satellites[item].meta.Status"
+      >
+        <div class="sat__name">
+          {{ satellites[item].meta.Name }}
+        </div>
+        <div class="sat__id">{{ satellites[item].catalog_id }}</div>
+        <div class="sat__actions">
+          <Button :on-click="showSatelliteDetails">
+            <Icon id="info" name="info" />
+          </Button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+import { mapMutations } from 'vuex'
+import Button from '~/components/global/Button.vue'
+import Icon from '~/components/global/Icon.vue'
+
+export default {
+  name: 'FocusList',
+  components: {
+    Button,
+    Icon
+  },
+  computed: {
+    focusedItems() {
+      return new Set(this.focusedSatellites)
+    },
+    satellites() {
+      return this.$store.state.satellites.satellites
+    },
+    ...mapGetters({
+      focusedSatellites: 'satellites/focusedSatellites',
+      focusedSatellitesCount: 'satellites/focusedSatellitesCount'
+    })
+  },
+  methods: {
+    removeFromFocused(e, catalog_id) {
+      this.focusedItems.delete(catalog_id)
+      this.updateFocusedSatellites(this.focusedItems)
+    },
+    addToFocused(e, catalog_id) {
+      this.focusedItems.add(catalog_id)
+      this.updateFocusedSatellites(this.focusedItems)
+    },
+    showSatelliteDetails() {
+      console.log('show details')
+    },
+    ...mapMutations({
+      updateFocusedSatellites: 'satellites/updateFocusedSatellites'
+    })
+  }
+}
 </script>
 
 <style lang="scss"></style>
