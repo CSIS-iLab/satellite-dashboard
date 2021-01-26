8ee0462f97e39df42301e8c6f342e233664ef339
Jacque Schrag <jnschrag@gmail.com>
2021-01-26 11:15:24 -0500

    Style timeline legend

--- components/timeline/Timeline.vue
+++ components/timeline/Timeline.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="timeline-container">
-    <div class="timeline-control">
+  <div class="timeline__container">
+    <div class="timeline__speed">
+      <span class="timeline-label-header">Speed</span>
+      <span class="timeline-label-value">
+        <v-select
+          v-model="chosenPlaybackSpeed"
+          :clearable="false"
+          :options="playbackSpeeds"
+          :menu-props="{ top: true, offsetY: true }"
+          @input="selectPlaybackSpeed"
+        ></v-select>
+      </span>
+    </div>
+    <div class="timeline-control-button-container">
+      <button class="timeline-control-button" @click="playOrPause">
+        {{ isPlaying ? 'playing' : 'paused' }}
+      </button>
+    </div>
+    <!-- <div class="timeline-control">
       <div class="timeline-control-labels">
         <span class="timeline-label-header">Speed</span>
         <span class="timeline-label-value">
@@ -18,7 +35,7 @@
           {{ isPlaying ? 'playing' : 'paused' }}
         </button>
       </div>
-    </div>
+    </div> -->
     <div class="timeline-series">
       <div class="timeline-current-value">
         <div class="timeline-value-change-back"></div>
@@ -41,7 +58,7 @@
       </div>
       <div ref="scrubber" class="timeline-scrub"></div>
     </div>
-    <div class="timeline-control">
+    <div class="timeline__scale">
       <div class="timeline-control-labels">
         <span class="timeline-label-header">Scale</span>
         <span class="timeline-label-value">
@@ -55,15 +72,13 @@
         </span>
       </div>
     </div>
-    <div class="timeline-key">
-      <ul>
-        <li>Payload/active</li>
-        <li>Payload/inactive</li>
-        <li>Rocket Body</li>
-        <li>Debris</li>
-        <li>Uncategorized</li>
-      </ul>
-    </div>
+    <ul class="timeline__legend" role="list">
+      <li data-status="active">Payload/active</li>
+      <li>Payload/inactive</li>
+      <li>Rocket Body</li>
+      <li>Debris</li>
+      <li>Uncategorized</li>
+    </ul>
   </div>
 </template>
 
@@ -184,123 +199,5 @@ export default {
 
 <style lang="scss">
 @import 'vue-select/src/scss/vue-select';
-
-.timeline-container {
-  display: flex;
-  min-height: 300px;
-  margin-top: 2em;
-  color: #fff;
-  background: rgba(0, 0, 0, 1);
-}
-
-.timeline-series {
-  flex-grow: 1;
-}
-
-.timeline-scrub {
-  position: relative;
-  height: 30px;
-}
-
-.timeline-control {
-  display: flex;
-  margin-top: 50px;
-}
-
-.timeline-control-labels {
-  display: flex;
-  flex-direction: column;
-  padding: 0 1em;
-}
-
-.timeline-label-header {
-  color: rgba(128, 128, 128, 0.8);
-  text-transform: uppercase;
-}
-
-.timeline-control-button {
-  width: 4em;
-  height: 4em;
-  margin: 0 1em;
-  background: rgba(128, 128, 128, 0.5);
-  border: 0;
-  border-radius: 50%;
-  cursor: pointer;
-}
-
-.timeline-series {
-  position: relative;
-  flex-grow: 1;
-}
-
-.timeline-current-value {
-  margin: 0 auto;
-  padding: 0.5em;
-  text-align: center;
-}
-
-.timeline-key {
-  flex-grow: 0.5;
-  font-size: 0.8em;
-}
-
-.timeline-key ul {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  height: 100px;
-  margin: 0 0 0 1em;
-}
-
-.timeline-container .datepicker-dark-calendar {
-  bottom: 0;
-  padding: 1em;
-  background: #222;
-  border: 0;
-}
-
-.timeline-container .datepicker-dark-input {
-  width: 8em;
-  padding: 0 1em;
-  color: #fff;
-  background: #222;
-  border: 0;
-}
-
-.timeline-container .datepicker-dark-input:focus {
-  background: #333;
-  outline: 0;
-}
-
-.timeline-container .datepicker-dark-calendar .up:not(.disabled):hover {
-  background: #444;
-}
-
-.timeline-container .vs__selected {
-  color: #ddd;
-}
-
-.cesium-viewer-animationContainer {
-  display: none;
-}
-
-.cesium-viewer .cesium-widget-credits {
-  display: none !important;
-}
-
-.cesium-timeline-bar {
-  height: 2.5em;
-}
-
-.timeline-series .vdp-datepicker {
-  position: absolute;
-  top: 0;
-  width: 100%;
-  text-align: center;
-}
-
-.timeline-series .vdp-datepicker input {
-  cursor: pointer;
-  opacity: 0;
-}
+@import '../assets/css/components/timeline';
 </style>
