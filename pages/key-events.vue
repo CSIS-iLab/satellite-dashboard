77b9d5e2b1159dce767d9d5f96c397fe71dd638c
Jacque Schrag <jnschrag@gmail.com>
2021-01-29 17:45:45 -0500

    Add about & methodology pages.

---
+++ pages/key-events.vue
@@ -0,0 +1,25 @@
+<template>
+  <Page title="Key Events" :content-has-bg="true">
+    <!-- eslint-disable-->
+    <section class="post__content entry-content">
+      <p>Coming soon.</p>
+    </section>
+    <!-- eslint-enable -->
+  </Page>
+</template>
+
+<script>
+import Page from '~/layout/page'
+
+export default {
+  layout: 'layout',
+  components: {
+    Page
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/css/components/post-component';
+@import '../assets/css/pages/post-content';
+</style>
