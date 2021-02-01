b164cc2d2037479f25f10d8cdd186a8818f7e01b
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2021-02-01 14:21:02 -0500

    Initial styling of help modal closes #49

--- components/visualizer/HelpPanel.vue
+++ components/visualizer/HelpPanel.vue
@@ -20,8 +20,63 @@
 
       <div class="help-panel__content">
         <TabPanel tab="about" scope="helpPanel">
-          About the interface content here.<br />
-          <Icon id="tilt-hand2" name="tilt-hand2" />
+          <h4 class="help-title">Pan</h4>
+          <div class="help-layout">
+            <div class="help-icon-layout">
+              <Icon id="pan-hand" name="pan-hand" />
+              <span class="help-icon-desc">
+                Drag one finger
+              </span>
+            </div>
+            <div class="help-icon-layout">
+              <Icon id="pan-mouse" name="pan-mouse" />
+              <span class="help-icon-desc">
+                Left click + drag
+              </span>
+            </div>
+          </div>
+
+          <h4 class="help-title help-title-line">Zoom</h4>
+          <div class="help-layout">
+            <div class="help-icon-layout">
+              <Icon id="zoom-hand" name="zoom-hand" />
+              <span class="help-icon-desc">
+                Pinch fingers
+              </span>
+            </div>
+            <div class="help-icon-layout">
+              <Icon id="zoom-mouse" name="zoom-mouse" />
+              <span class="help-icon-desc">
+                Right click + drag, or scroll
+              </span>
+            </div>
+          </div>
+
+          <h4 class="help-title help-title-line">Rotate</h4>
+          <div class="help-layout">
+            <div class="help-icon-layout">
+              <Icon id="rotate-hand" name="rotate-hand" />
+              <span class="help-icon-desc">
+                Drag fingers in different directions
+              </span>
+            </div>
+            <div class="help-icon-layout">
+              <Icon id="rotate-mouse" name="rotate-mouse" />
+              <span class="help-icon-desc">
+                Middle click + drag or CTRL + Left/Right click + drag
+              </span>
+            </div>
+          </div>
+
+          <h4 class="help-title help-title-line">Tilt</h4>
+          <div class="help-layout">
+            <div class="help-icon-layout">
+              <Icon id="tilt-hand" name="tilt-hand" />
+              <span class="help-icon-desc">
+                Drag two fingers
+              </span>
+            </div>
+          </div>
         </TabPanel>
         <TabPanel tab="view" scope="helpPanel">
           Changing the view goes here.
@@ -53,4 +108,6 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import '../assets/css/components/help-panel';
+</style>
