500c06481eb7da97db485126ffb0264273334281
jdromero88 <jdromerov88@gmail.com>
2022-01-21 15:45:08 -0500

    corrected data loaded to false

--- components/visualizer/MagicChart.vue
+++ components/visualizer/MagicChart.vue
@@ -73,7 +73,7 @@ export default {
   },
   data() {
     return {
-      dataLoaded: true,
+      dataLoaded: false,
       sourceInfo: [],
       chartOptions: {
         title: { text: 'Historical Longitudes' },
