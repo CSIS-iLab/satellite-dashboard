0cd5016d45676416e43dff33978a6a2142bbf4be
jdromero88 <jdromerov88@gmail.com>
2022-02-22 18:05:05 -0500

    save workikng in progress to fix the day

--- components/visualizer/FocusList.vue
+++ components/visualizer/FocusList.vue
@@ -199,7 +199,7 @@ export default {
       const formattedDate = [
         date.getFullYear(),
         ('0' + (date.getMonth() + 1)).slice(-2),
-        ('0' + date.getDate()).slice(-2)
+        ('0' + (date.getDate() + 1)).slice(-2)
       ].join('-')
 
       viewParams.set('date', formattedDate)
