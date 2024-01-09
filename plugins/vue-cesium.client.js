8e58dccb111a7ceea6a5ce3c9d6a4b565fa34fc3
jdromero88 <jdromerov88@gmail.com>
2024-01-09 12:07:21 -0500

    Update cesium.js version to show the globe

--- plugins/vue-cesium.client.js
+++ plugins/vue-cesium.client.js
@@ -4,7 +4,7 @@ import lang from 'vue-cesium/lang/en-us'
 
 Vue.use(VueCesium, {
   lang,
-  cesiumPath: 'https://unpkg.com/cesium@1.84.0/Build/Cesium/Cesium.js',
+  cesiumPath: 'https://unpkg.com/cesium@1.113.0/Build/Cesium/Cesium.js',
   accessToken:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZjJhOGJjMi03NDAyLTRmOTEtOGM0OC05ZDZlYWRjN2Q5ZWYiLCJpZCI6MjkwMjUsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1OTE3MjI4NzV9.yMqS4rU-P4ZXfth_2U3Tawn3KQ9C4AXX9pXuWj3_l4Q'
 })
